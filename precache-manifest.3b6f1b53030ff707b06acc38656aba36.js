c9f4dd2e85434a826e3361d238a80e6c9d584da4
bluedinosaur <64344780+bluedinosaur@users.noreply.github.com>
2020-07-03 14:18:21 +1000

    Updates

---
+++ precache-manifest.3b6f1b53030ff707b06acc38656aba36.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "992702742fd41824db84f8df1913e805",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "7fa60c1d1f939e6d04a2",
+    "url": "/contactmanager/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "22f200c5bedae2e68152",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "7fa60c1d1f939e6d04a2",
+    "url": "/contactmanager/static/js/2.b7bc61c2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contactmanager/static/js/2.b7bc61c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22f200c5bedae2e68152",
+    "url": "/contactmanager/static/js/main.d3d6091f.chunk.js"
+  },
+  {
+    "revision": "78370bb576dc7a6cd44e",
+    "url": "/contactmanager/static/js/runtime-main.cdf6a9fb.js"
+  }
+]);
